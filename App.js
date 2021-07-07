daad6f1161d579d519f7c160cf7d18d8a16f92c4
pjoon357 <40589394+pjoon357@users.noreply.github.com>
2021-07-07 19:14:57 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {Alert} from 'react-native';
 import * as Location from 'expo-location';
 import axios from 'axios';
 
-const API_KEY = "8c2e23859264b2354dc719b8b0338e80";
+const API_KEY = "";
 
 export default class extends React.Component {
   state= {
