e7869e13f3d5b3b9f7c59d34c6351cfefa10ce47
ByungJun <pjoon357@naver.com>
2021-07-07 17:29:31 +0900

    second-day

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+import {Alert} from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hihi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "8c2e23859264b2354dc719b8b0338e80";
+
+export default class extends React.Component {
+  state= {
+    isLoading: true
+  }
+
+  getWeather = async (latitude,longitude) =>{
+    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}
+    `);
+    console.log(data);
+  }
+
+  getLocation = async () =>{
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude,longitude);
+      this.setState({isLoading: true});
+    } catch (error) {
+      Alert.alert("Can't find you","so sad...");
+    }
+  }
+  
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render (){
+    const {isLoading} = this.state;
+    return isLoading ? <Loading/> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # weather-app
 Learning React Native
+
+## 박병준의 React Native 도전기
+1. 1일차 Reauirement&react 생성
+2. 2일차 Logic&API
